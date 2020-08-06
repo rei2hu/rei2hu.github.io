bd1a0b7ea18cdf6f434d70e19a92471f846c525a
reimu <r2hkri@gmail.com>
2020-08-06 00:04:49 -0500

    aws

--- src/scripts/playful_reimu.js
+++ src/scripts/playful_reimu.js
@@ -16,26 +16,6 @@
   function $i(id) {
     return document.getElementById(id);
   }
-  function $r(parent, child) {
-    document.getElementById(parent).removeChild(document.getElementById(child));
-  }
-  function $t(name) {
-    return document.getElementsByTagName(name);
-  }
-  function $c(code) {
-    return String.fromCharCode(code);
-  }
-  function $h(value) {
-    return (
-      "0" + Math.max(0, Math.min(255, Math.round(value))).toString(16)
-    ).slice(-2);
-  }
-  function _i(id, value) {
-    $t("div")[id].innerHTML += value;
-  }
-  function _h(value) {
-    return !hires ? value : Math.round(value / 2);
-  }
 
   function get_screen_size() {
     var w = document.documentElement.clientWidth;
@@ -43,72 +23,81 @@
     return Array(w, h);
   }
 
-  var url = document.location.href;
+  // number of stars
+  var n = 64;
 
-  var flag = true;
-  var test = true;
-  var n = 256;
   var w = 0;
   var h = 0;
+
   var x = 0;
   var y = 0;
   var z = 0;
+
   var star_color_ratio = 0;
   var star_x_save;
   var star_y_save;
-  var star_ratio = 256;
-  var star_speed = 1;
-  var star_speed_save = 0;
+  var star_ratio = 64;
+  var star_speed = 0.5;
   var star = new Array(n);
-  var color;
-  var opacity = 0.1;
 
   var cursor_x = 0;
   var cursor_y = 0;
-  var mouse_x = 0;
-  var mouse_y = 0;
 
   var canvas_x = 0;
   var canvas_y = 0;
-  var canvas_w = 0;
-  var canvas_h = 0;
   var context;
 
-  var key;
-  var ctrl;
-
-  var timeout;
-  var fps = 0;
+  var lastAnim;
+  var fadein = 0;
 
   function init() {
-    var a = 0;
     for (var i = 0; i < n; i++) {
       star[i] = new Array(5);
+
+      // 
       star[i][0] = Math.random() * w * 2 - x * 2;
       star[i][1] = Math.random() * h * 2 - y * 2;
       star[i][2] = Math.round(Math.random() * z);
+
+      // position
       star[i][3] = 0;
       star[i][4] = 0;
     }
+
     var starfield = $i("gate");
     starfield.style.position = "fixed";
     starfield.width = w;
     starfield.height = h;
     context = starfield.getContext("2d");
-    // context.lineCap='round';
     context.fillStyle = "rgb(255,255,255)";
     context.strokeStyle = "rgb(0,0,0)";
   }
 
   function anim() {
-    mouse_x = cursor_x - x;
-    mouse_y = cursor_y - y;
+    var temp = lastAnim;
+    lastAnim = Date.now();
+    var elapsed = (lastAnim - temp) / 30;
+
+    if (fadein < 1) {
+      fadein += 0.01 * elapsed;
+      var fadein_color = (255 * (1 - fadein)) | 0;
+      context.strokeStyle = "rgb(" +
+        fadein_color + "," +
+        fadein_color + "," +
+        fadein_color + ")";
+    }
+
+    var mouse_x = cursor_x - x;
+    var mouse_y = cursor_y - y;
     context.fillRect(0, 0, w, h);
+
     for (var i = 0; i < n; i++) {
-      test = true;
+      var test = true;
+      
       star_x_save = star[i][3];
       star_y_save = star[i][4];
-      star[i][0] += mouse_x / w * 100;
+
+      star[i][0] += mouse_x / w * 100 * elapsed;
       if (star[i][0] > x << 1) {
         star[i][0] -= w << 1;
         test = false;
@@ -117,7 +106,8 @@
         star[i][0] += w << 1;
         test = false;
       }
-      star[i][1] += mouse_y / w * 100;
+
+      star[i][1] += mouse_y / w * 100 * elapsed;
       if (star[i][1] > y << 1) {
         star[i][1] -= h << 1;
         test = false;
@@ -126,7 +116,8 @@
         star[i][1] += h << 1;
         test = false;
       }
-      star[i][2] -= star_speed;
+
+      star[i][2] -= star_speed * elapsed;
       if (star[i][2] > z) {
         star[i][2] -= z;
         test = false;
@@ -135,8 +126,13 @@
         star[i][2] += z;
         test = false;
       }
+
+
+
+      // new positions = center + x speed
       star[i][3] = x + (star[i][0] / star[i][2]) * star_ratio;
       star[i][4] = y + (star[i][1] / star[i][2]) * star_ratio;
+
       if (
         star_x_save > 0 &&
         star_x_save < w &&
@@ -152,7 +148,9 @@
         context.closePath();
       }
     }
-    timeout = setTimeout(anim, fps);
+
+    // timeout = setTimeout(anim, fps);
+    timeout = window.requestAnimationFrame(anim);
   }
 
   function move(evt) {
@@ -161,80 +159,16 @@
     cursor_y = evt.pageY - canvas_y - window.scrollY;
   }
 
-  function key_manager(evt) {
-    evt = evt || event;
-    key = evt.which || evt.keyCode;
-    // ctrl=evt.ctrlKey;
-    switch (key) {
-      case 27:
-        flag = !flag;
-        if (flag) {
-          timeout = setTimeout(anim, fps);
-        } else {
-          clearTimeout(timeout);
-        }
-        break;
-      case 32:
-        star_speed_save = star_speed != 0 ? star_speed : star_speed_save;
-        star_speed = star_speed != 0 ? 0 : star_speed_save;
-        break;
-      case 13:
-        context.fillStyle = "rgba(0,0,0," + opacity + ")";
-        break;
-    }
-    top.status = "key=" + (key < 100 ? "0" : "") + (key < 10 ? "0" : "") + key;
-  }
-
-  function release() {
-    switch (key) {
-      case 13:
-        context.fillStyle = "rgb(0,0,0)";
-        break;
-    }
-  }
-
-  function mouse_wheel(evt) {
-    evt = evt || event;
-    var delta = 0;
-    if (evt.wheelDelta) {
-      delta = evt.wheelDelta / 120;
-    } else if (evt.detail) {
-      delta = -evt.detail / 3;
-    }
-    star_speed += delta >= 0 ? -0.2 : 0.2;
-    if (evt.preventDefault) evt.preventDefault();
-  }
-
   function start() {
     resize();
+    lastAnim = Date.now();
     anim();
   }
 
   function resize() {
-    w = parseInt(
-      url.indexOf("w=") != -1
-        ? url.substring(
-            url.indexOf("w=") + 2,
-            url.substring(url.indexOf("w=") + 2, url.length).indexOf("&") != -1
-              ? url.indexOf("w=") +
-                  2 +
-                  url.substring(url.indexOf("w=") + 2, url.length).indexOf("&")
-              : url.length
-          )
-        : get_screen_size()[0]
-    );
-    h = parseInt(
-      url.indexOf("h=") != -1
-        ? url.substring(
-            url.indexOf("h=") + 2,
-            url.substring(url.indexOf("h=") + 2, url.length).indexOf("&") != -1
-              ? url.indexOf("h=") +
-                  2 +
-                  url.substring(url.indexOf("h=") + 2, url.length).indexOf("&")
-              : url.length
-          )
-        : get_screen_size()[1]
-    );
+    var sizes = get_screen_size();
+    w = sizes[0];
+    h = sizes[1];
     x = Math.round(w / 2);
     y = Math.round(h / 2);
     z = (w + h) / 2;
@@ -245,6 +179,7 @@
   }
 
   document.onmousemove = move;
-  start();
   document.body.onresize = resize;
+  // start();
+  setTimeout(start, 5000);
 })();
