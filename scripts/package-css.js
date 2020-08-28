afd3c99c7acdfe470a3007477767c12438f7f47d
reimu <reimu@reimu.ws>
2020-08-27 20:54:21 -0500

    new post 18 and updated scripts

--- scripts/package-css.js
+++ scripts/package-css.js
@@ -3,15 +3,19 @@ const path = require("path");
 const CleanCss = require("clean-css");
 
 const files = fs.readdirSync(path.resolve("src", "style"));
-const toMove = files.filter((file) => file.endsWith(".css") || file.endsWith(".ttf"));
+const toMove = files.filter(
+  (file) => file.endsWith(".css") || file.endsWith(".ttf")
+);
 
 const cleaner = new CleanCss({});
 
 fs.promises.mkdir("built/style", { recursive: true }).then(() => {
-  toMove
-    .forEach((file) => {
-      const contents = fs.readFileSync(path.resolve("src", "style", file));
-      const minified = cleaner.minify(contents);
-      fs.writeFileSync(path.resolve("built", "style", file), minified.warnings.length ? contents : minified.styles);
-    });
+  toMove.forEach((file) => {
+    const contents = fs.readFileSync(path.resolve("src", "style", file));
+    const minified = cleaner.minify(contents);
+    fs.writeFileSync(
+      path.resolve("built", "style", file),
+      minified.warnings.length ? contents : minified.styles
+    );
+  });
 });
