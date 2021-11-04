7ad8a5f334806e6e5829cdb1077297dc05f9f037
reimu <reimu@reimu.ws>
2021-11-03 19:44:22 -0500

    keep script processing consistent

--- scripts/build-options.js
+++ scripts/build-options.js
@@ -10,10 +10,11 @@ module.exports = {
     removeComments: true,
   },
   obfusJsOpts: {
+    simplify: true,
     compact: true,
+    stringArray: false,
+    identifierNamesGenerator: "mangled", // makes variable naming consistent
     // disableConsoleOutput: true,
-    // identifierNamesGenerator: "hexadecimal",
-    // stringArray: true,
     // stringArrayEncoding: "base64",
     // stringArrayThreshold: 0.75,
     // transformObjectKeys: true,
