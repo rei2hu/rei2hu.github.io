d2930ef75efb79ac67eb6595536c7221df146879
reimu <reimu@reimu.ws>
2023-11-05 16:09:05 -0600

    cleanup and improve email obfuscation

--- src/scripts/playful_reimu.js
+++ src/scripts/playful_reimu.js
@@ -4,27 +4,21 @@
 
 	function bottomRightLinks() {
 		var div = document.getElementById("e");
-		var content = "email: (domain without .extension) @ (domain)";
+		var content = "(domain without .extension) @ (domain)";
 		div.addEventListener("mouseenter", function () {
-			div.innerHTML =
-				"email: " +
-				atob(
-					"cmVpbXUgKGF" +
-						(0x10 & 0) +
-						"KSByZWltdSBkb" +
-						(6 - 3) +
-						"Qgd" +
-						(1 + 2) +
-						"M="
-				);
+			div.innerHTML = atob(
+				"cmVpbXUgKGF" +
+					(0x10 & 0) +
+					"KSByZWltdSBkb" +
+					(6 - 3) +
+					"Qgd" +
+					(1 + 2) +
+					"M="
+			);
 		});
 		div.addEventListener("mouseleave", function () {
 			div.innerHTML = content;
 		});
-		div.style.bottom = "1.5rem";
-		div.style.right = "1.5rem";
-		div.style.position = "fixed";
-		div.style.textAlign = "right";
 		div.style.zIndex = 1;
 		div.innerHTML = content;
 		document.body.appendChild(div);
