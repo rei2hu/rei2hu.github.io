fc4d2e83dc32f299041b72f0ff1a2223ef42ec69
reimu <reimu@reimu.ws>
2024-07-03 08:55:14 -0500

    make lights respond to color theme

--- src/scripts/lights.js
+++ src/scripts/lights.js
@@ -1,9 +1,21 @@
 (() => {
 	const LIGHTS_KEY = "LIGHTS_KEY";
-	const value = localStorage.getItem(LIGHTS_KEY);
 	const lights = document.getElementById("lights");
 
-	lights.checked = value === "true";
+	const matchQuery = window.matchMedia("(prefers-color-scheme: light)");
+	const checkLights = (query) => {
+		// default to color scheme if there is nothing saved
+		// only need to check against light mode because dark is default
+		if (localStorage.getItem(LIGHTS_KEY) === null) {
+			lights.checked = query.matches;
+		} else {
+			lights.checked = localStorage.getItem(LIGHTS_KEY) === "true";
+		}
+	};
+	checkLights(matchQuery);
+	matchQuery.addEventListener("change", checkLights);
+
+	// update settings when checkbox is changed
 	lights.addEventListener("change", () => {
 		localStorage.setItem(LIGHTS_KEY, lights.checked);
 	});
